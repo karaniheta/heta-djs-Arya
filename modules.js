1565521da0cb0950dfe43c845bebc077024f1406
heta-djs-arya <karaniheta@gmail.com>
2024-10-14 19:30:25 +0530

    'task_1'

---
+++ modules.js
@@ -0,0 +1,17 @@
+/*console.log(__dirname)
+setInterval(() => {
+    console.log('hii')
+}, 2000);*/
+
+
+//modules
+const names = require('./names');
+const sayHi = require('./utils')
+const data = require('./alternative-flavor')
+require('./mindGrenade')
+
+
+
+sayHi('pankti')
+sayHi(names.heta)
+sayHi(names.hdk)
---
+++ names.js
@@ -0,0 +1,10 @@
+
+//local
+const secret='super secret'
+
+//share
+const heta='heta'
+const hdk = 'hdk'
+
+
+module.exports = {hdk,heta}
